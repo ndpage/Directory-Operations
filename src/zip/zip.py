27f5f615ab45ea434f084b2fc8df6d304b5faf58
Page Nathan (DC-MH/ENG1.13-NA) <Nathan.Page@boschrexroth-us.com>
2021-12-21 13:45:33 -0500

    Added OSError exception to handle errors

--- src/zip/zip.py
+++ src/zip/zip.py
@@ -1,12 +1,19 @@
 import shutil
 from os.path import exists
-
-
+import sys
 
 def zip_dir(dir):
-    dir = str(dir)
-    shutil.make_archive(dir, 'zip', dir)
+    
+    try:
+        dir = str(dir)
+        shutil.make_archive(dir, 'zip', dir)
+        zipped = exists(dir+'.zip')
 
-    file_exists = exists(dir+'.zip')
+    # Basic error handling for displaying the error number and message
+    except OSError as err:
+        print(">>   OS error: {0}".format(err))
+        zipped = False
+        
+    return zipped
 
-    return file_exists
+zip_dir('C:/test')
